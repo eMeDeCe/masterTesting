62c1d241503f7257b958f92297cd72cbd12b336f
Mariangel <maria.angel.delacueva@gmail.com>
2021-01-10 20:53:40 +0100

    Add cypress test to login

---
+++ cypress/integration/login.spec.ts
@@ -0,0 +1,38 @@
+import { literals } from 'core/i18n';
+describe('Login specs', () => {
+    it('visit the login page', () => {
+      cy.visit('/');
+    });
+
+    it('should name input has the focus when it clicks on it', () => {
+      // Arrange
+      // Act
+      cy.visit('/');
+      cy.get('input[name="user"]').click();
+  
+      // Assert
+      cy.get('input[name="user"]').should('have.focus');
+    });
+
+    it('should show an alert with a message when type invalid credentials', () => {
+      // Arrange
+      const user = 'admin';
+      const password = '1234';
+
+      // Act
+      cy.visit('/');
+      cy.get('input[name="user"]').as('userInput');
+      cy.get('input[name="password"]').as('passwordInput');
+      
+      cy.get('@userInput').type(user);
+      cy.get('@passwordInput').type(password);
+      cy.findByRole('button', { name: 'Login' }).click();
+      
+      // Assert
+      cy.get('@userInput').should('have.value', user);
+      cy.get('@passwordInput').should('have.value', password);
+      cy.findByText(
+        'Usuario y/o password no válidos'
+      );
+    });
+});
